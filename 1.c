cc8c2034cfc58ca1271394d0a455a9e760358e15
ilsang_Son <son2717@gmail.com>
2024-05-07 16:42:30 +0900

    1
    1

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyName1(void)
+{
+    printf("1.my name is [name]\n");
+    return 1;
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myProject.h"
+int main(void)
+{
+    printf("Chief name is [팀장이름]\n");
+    printMyName1();
+    printMyName2();
+    printMyName3();
+    printMyName4();
+}
